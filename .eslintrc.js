6db92507a79cc71de305f106d52bf254afd1d53c
Alvaro Parada <alvaro.parada92@gmail.com>
2023-02-16 10:48:19 +0100

    Initial commit
    Generated by create-expo-app 1.3.2.

---
+++ .eslintrc.js
@@ -0,0 +1,90 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "xo",
+    "prettier",
+    "plugin:react-native/all",
+  ],
+  overrides: [
+    {
+      extends: ["xo-typescript", "prettier"],
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/consistent-type-definitions": [
+          "error",
+          "interface",
+        ],
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-var-requires": "off",
+        // "@typescript-eslint/no-unsafe-return": "off",
+        "@typescript-eslint/no-require-imports": "off",
+        "@typescript-eslint/indent": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "react-native/no-color-literals": ["off"],
+        "no-unused-vars": [
+          "error",
+          {
+            vars: "all",
+            args: "after-used",
+            ignoreRestSiblings: true,
+            argsIgnorePattern: /^/.source,
+            caughtErrors: "all",
+            caughtErrorsIgnorePattern: /^$/.source,
+          },
+        ],
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "prettier",
+    "jest",
+    "testing-library",
+  ],
+  rules: {
+    "no-multi-spaces": ["error"],
+    "no-multiple-empty-lines": ["error", { max: 1 }],
+    "object-curly-spacing": ["error", "always"],
+    "no-use-before-define": ["off"],
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "@typescript-eslint/naming-convention": [
+      "warn",
+      {
+        selector: "interface",
+        format: ["PascalCase"],
+        custom: {
+          regex: "^I[A-Z]",
+          match: true,
+        },
+      },
+    ],
+    "react-native/no-inline-styles": ["error"],
+    "react-native/no-raw-text": ["warn"],
+    "react-native/split-platform-components": ["warn"],
+    "react-native/sort-styles": "off",
+    "react-native/no-color-literals": ["warn"],
+    "react-native/no-unused-styles": ["warn"],
+    "comma-dangle": ["error", "only-multiline"],
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
+    "no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: true,
+        argsIgnorePattern: /^/.source,
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: /^$/.source,
+      },
+    ],
+  },
+};
---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>Gotta catch'em all!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default App;
